86709f5868a6e1f49a0a6195c8d1aca99ddfd2ae
hmsong <114149676+songhyeonmin@users.noreply.github.com>
2025-03-14 13:21:01 +0900

    Update 실습1.cpp

--- 실습1.cpp
+++ 실습1.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int n;
-    int sum;
-    int list[] {1,2,3,4,5};
-
-    cout<<"100이하의 자연수 n을 입력하세요"<< endl;
-    cin >> n;
-    
-    for(int i : list){
-        cout<< n*i;
-    }
-    return 0;
-}
+202401293 컴퓨터공학부 송현민
